f4b9041b97ad298be415c7001f3aa58f6f5628d8
ranon rat <66473662+ranon-rat@users.noreply.github.com>
2020-10-24 04:08:41 -0500

    Add files via upload

--- main.go
+++ main.go
@@ -3,36 +3,33 @@ package main
 import (
 	"bytes"
 	"image/png"
-	"log"
 	"net/http"
 	"strconv"
 
+	"github.com/gorilla/mux"
 	"github.com/kbinani/screenshot"
 )
 
 func sendI(w http.ResponseWriter, r *http.Request) {
-
 	n := screenshot.NumActiveDisplays()
-
 	for i := 0; i < n; i++ {
 		bounds := screenshot.GetDisplayBounds(i)
 		img, _ := screenshot.CaptureRect(bounds)
 		buffer := new(bytes.Buffer)
 		if err := png.Encode(buffer, img); err != nil {
-			log.Println("unable to encode image.")
+
 		}
 		w.Header().Set("Content-Type", "image/jpeg")
 		w.Header().Set("Content-Length", strconv.Itoa(len(buffer.Bytes())))
 		if _, err := w.Write(buffer.Bytes()); err != nil {
-			log.Println("unable to write image.")
+
 		}
 
 	}
 }
-
 func main() {
-	http.HandleFunc("/image", sendI)
-	http.Handle("/", http.FileServer(http.Dir("./page")))
-
-	http.ListenAndServe(":8090", nil)
+	r := mux.NewRouter()
+	r.HandleFunc("/image/{a}", sendI)
+	r.Handle("/", http.FileServer(http.Dir("./page")))
+	http.ListenAndServe(":8090", r)
 }
